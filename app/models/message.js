506b32c3fa3ad9410a1f2a0757db53bf7fc12bee
Lars Bünting <lars.buenting@smacc.io>
2017-01-16 07:24:26 +0100

    fix tests

---
+++ app/models/message.js
@@ -0,0 +1,11 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  body: attr('string'),
+  author: belongsTo('user'),
+  room: belongsTo('room'),
+  insertedAt: attr('date'),
+  updatedAt: attr('date')
+});
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,12 @@
 import DS from 'ember-data';
 
+const {attr, hasMany} = DS;
+
 export default DS.Model.extend({
-  email: DS.attr('string'),
-  password: DS.attr('string'),
-  passwordConfirmation: DS.attr('string'),
-  rooms: DS.hasMany('room')
+  email: attr('string'),
+  password: attr('string'),
+  passwordConfirmation: attr('string'),
+  gravatar: attr('string'),
+  rooms: hasMany('room'),
+  messages: hasMany('message')
 });
