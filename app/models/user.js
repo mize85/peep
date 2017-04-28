5f77be190f009d223a244860b9098080f5a4e8c1
Lars Bünting <lars.buenting@smacc.io>
2017-04-28 09:54:54 +0200

    add some styles

--- app/models/user.js
+++ app/models/user.js
@@ -1,19 +1,14 @@
 import DS from 'ember-data';
-import Ember from 'ember';
-
 const {attr, hasMany} = DS;
-const {computed} = Ember;
 
 export default DS.Model.extend({
   email: attr('string'),
   password: attr('string'),
   passwordConfirmation: attr('string'),
   avatar: attr('file'),
+  avatarUrl: attr('string'),
+  thumbUrl: attr('string'),
+
   rooms: hasMany('room'),
-  messages: hasMany('message'),
-
-
-  avatarUrl: computed('avatar.file_name', function(){
-    return this.get('avatar.file_name') ? `/uploads/${this.get('avatar.file_name')}` : null;
-  })
+  messages: hasMany('message')
 });
